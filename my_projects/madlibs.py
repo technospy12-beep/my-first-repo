6842f06097b40d87918c9c7b59b2ee7d2d89f349
technospy <technospy12@gmail.com>
2025-10-18 21:39:32 +0530

    Auto commit 2025-10-18 21:39:32.118718

--- my_projects/madlibs.py
+++ my_projects/madlibs.py
@@ -8,3 +8,5 @@ print(f"today i went to {adjective1} zoo")
 print(f"in an exhibition, i saw a {noun1}")
 print(f"{noun1} was {adjective2} and {verb1}")
 print (f"i was {adjective3}!")
+
+print("over")
