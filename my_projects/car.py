d2c7f3b5115605790da0bb5f152aed1d799d4487
technospy <technospy12@gmail.com>
2025-10-19 18:40:13 +0530

    Auto commit 2025-10-19 18:40:13.345790

---
+++ my_projects/car.py
@@ -0,0 +1,8 @@
+class Car:
+    def __init__(self,model,year,colour,for_sale):
+        self.model= model
+        self.year = year
+        self.coulour = colour
+        self.for_sale = for_sale
+
+
--- my_projects/object oriented programming.py
+++ my_projects/object oriented programming.py
@@ -1,11 +1,14 @@
-class Car:
-    def __init__(self,model,year,colour,for_sale):
-        self.model= model
-        self.year = year
-        self.coulour = colour
-        self.for_sale = for_sale
+from car import Car
 
-car1 = Car("lamborgini",20001,"yellow",True)
-print(car1)
+car1 = Car("lamborgini",20001,"yellow",False)
+car2 = Car("pollo",20002,"red",True)
+car3 = Car("ferrari",20003,"blue",True)
+
+
+
+print(car1.model)
+print(car1.year)
+print(car1.coulour)
+print(car1.for_sale)
 
 
