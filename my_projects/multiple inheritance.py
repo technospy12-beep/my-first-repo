992574aaa7d96ca36bc090e1b950ee4cf147436d
technospy <technospy12@gmail.com>
2025-10-19 22:57:28 +0530

    Auto commit 2025-10-19 22:57:27.964063

---
+++ my_projects/multiple inheritance.py
@@ -0,0 +1,26 @@
+class prey:
+    def flee(self):
+        print(f"this animal is fleeing")
+
+
+class predature:
+    def hunt(self):
+        print(f"this animal is hunting")
+
+class Rabbit(prey):
+    pass
+
+class hawk(predature):
+    pass
+
+class Fish(prey,predature):
+    pass
+
+
+
+rabbit = Rabbit()
+hawk = hawk()
+fish = Fish()
+
+
+hawk.hunt()
