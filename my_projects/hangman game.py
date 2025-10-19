1494bda5bf6ff16bb76566e97af7e42b3080c4e8
technospy <technospy12@gmail.com>
2025-10-19 18:30:38 +0530

    Auto commit 2025-10-19 18:30:38.419514

---
+++ my_projects/hangman game.py
@@ -0,0 +1,6 @@
+import random
+
+
+words= ("apple","orange","banana","coconut","pineapple")
+for word in words:
+    print(word)
---
+++ my_projects/object oriented programming.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self,model,year,colour,for_sale):
+        self.model= model
+        self.year = year
+        self.coulour = colour
+        self.for_sale = for_sale
+
+car1 = Car("lamborgini",20001,"yellow",True)
+print(car1)
+
+
