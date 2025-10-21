f296bf9830b9b621bfd283ef8b3989697a8e2bb3
technospy <technospy12@gmail.com>
2025-10-21 16:58:50 +0530

    Auto commit 2025-10-21 16:58:50.617440

---
+++ my_projects/super.py
@@ -0,0 +1,19 @@
+class Circle:
+    def __init__(self,colour,filled,radius):
+        self.colour=colour
+        self.filled=filled
+        self.radius=radius
+
+
+class Square:
+    def __init__(self,colour,filled,width):
+        self.colour=colour
+        self.filled=filled
+        self.width=width
+
+
+class Triangle:
+    pass
+
+
+
